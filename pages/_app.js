f845fe74448ee78d82133afdbdfec0394d0dfeff
Binyam Sintayehu <73512283+Binyam-maker@users.noreply.github.com>
2022-09-20 10:36:42 +0300

    added loading slim bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,11 @@ import "../styles/globals.css";
 import { wrapper } from "../store";
 import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
-
+import NextNProgress from "nextjs-progressbar";
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <NextNProgress color="#f40612" />
       <Component {...pageProps} />
 
       {/* putting toastify in the root */}
